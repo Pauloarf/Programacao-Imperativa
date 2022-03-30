eab6fad0739380a4723cedd8e58dc1a3e632030f
Paulo Ferreira <pauloferreira.vnc@gmail.com>
2022-03-30 15:12:25 +0100

    Update 30/03/2022

--- 1-50 Questões/Ficha.c
+++ 1-50 Questões/Ficha.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 70 //Max array size
 
@@ -52,7 +53,7 @@ int qDig(unsigned int n){
 }
 
 /* This function is my version of the pre-define function *strcat in c*/
-char *mystrcat(char s1[], char s2[]){
+char *myStrcat(char s1[], char s2[]){
     int i, size;
     for(size = 0; s1[size] != '\0'; size++);
     for(i = 0; s2[i] != '\0'; i++, size++){
@@ -62,10 +63,55 @@ char *mystrcat(char s1[], char s2[]){
     return s1;
 }
 
+/* This function copys the string in source to dest*/
+char *myStrcpy(char *dest, char source[]){
+    int i;
+    for(i = 0; source[i] != '\0'; i++){
+        dest[i] = source[i];
+    }
+    dest[i] = '\0';
+    return dest;
+}
+
+/* This function tests if the strings are the same, otherwise it returns <0 for s1<s2*/
+/*                                                                    or >0 for s1>s2*/
+int myStrcmp(char s1[], char s2[]){
+    int i;
+    for(i = 0; s1[i] != '\0' && s2[i] != '\0'; i++){
+        if(s1[i] == s2[i]);
+        else break;
+    }
+    if(s1[i] < s2[i]) return -1;
+    if(s1[i] > s2[i]) return 1;
+    return 0;
+}
+
+/* This funtion returns where the s1 pointer starts if s2 happens inside s1*/
+char *myStrstr(char s1[], char s2[]){
+    int i;
+    char a = 1;
+    for(i = 0; s1[i] != '\0'; i++){
+        if(s1[i] == s2[0]){
+            int aux = i;
+            int j;
+            for(j = 0; s2[j] != '\0'; j++, aux++){
+                if(s1[aux] == s2[j]);
+                else break;
+            }
+            if(s2[j] == '\0'){
+                s1 = s1 + i;
+                return s1;
+            }
+        }
+    }
+    return NULL;
+}
+
 int main(){
     char s1[MAX] = "tentei, e no fim ";
     char s2[MAX] = "ate deu";
     int n, r;
+    char c;
     
     printf("Introduza um inteiro para obter informacao acerca do mesmo em base 2:\n");
     scanf("%d",&n);
@@ -81,6 +127,20 @@ int main(){
     printf("As strings: "); dumpV(s1); 
     printf("e           "); dumpV(s2);
     printf("concatenadas originam a string: ");
-    mystrcat(s1,s2); dumpV(s1);
-    return 0;
+    myStrcat(s1,s2); dumpV(s1);
+    
+    printf("\nA string s2 = "); dumpV(s2); 
+    printf("copiada para s1, transforma s1 em ");
+    myStrcpy(s1,s2); dumpV(s1);
+
+    r = myStrcmp(s1,s2);
+    printf("\nTestar igualdade das strings: "); dumpV(s1);
+    printf("e                             "); dumpV(s2);
+    printf("r = %d", r);
+
+    char ss1[MAX] = "s2 pertence a s1";
+    char ss2[MAX] = "pertence";
+    printf("\n\nstrstr de "); dumpV(ss2); 
+    printf("e         "); dumpV(ss1);
+    printf("e igual a {%s}",myStrstr(ss1,ss2));
 }
