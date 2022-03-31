95656e421236f063b70d8411d29b1f79e198a2a8
Paulo Ferreira <pauloferreira.vnc@gmail.com>
2022-03-31 20:08:14 +0100

    Update 31/03/2022

---
+++ Fichas/Ficha5/Alunos.c
@@ -0,0 +1,50 @@
+#include "Alunos.h"
+
+/* This function prints the content of an array*/
+void dumpV(int v[], int N){
+    int i;
+    for ( i = 0; i < N; i++) printf ("%d ", v[i]);
+}
+
+//Exercise 1
+int nota(Aluno a){
+    float n = 0, m = 0;
+    for(int i = 0; i < 6; i++){
+        m += a.miniT[i];
+    }
+    m *= 10;
+    m /= 6;
+    n = (m * 0.25) + (a.teste * 0.75);
+    return n;
+}
+
+/* This function prints the students*/
+void imprimeAluno(Aluno *a){
+    int i;
+    int x = nota(*a);
+    printf ("%-5d %s (%d", a->numero, a->nome, a->miniT[0]);
+    for(i = 1; i < 6; i++) printf (", %d", a->miniT[i]);
+    printf (") %5.2f (%d)\n", a->teste, x);
+}
+
+//Exercise 2
+int procuraNum(int num, Aluno t[], int N){
+    for(int i = 0; i < N; i++){
+        if(num == t[i].numero) return i;
+    }
+    return -1;
+}
+
+//Exercise 3
+void ordenaPorNum(Aluno t[], int N){
+    int i, j;
+    for(i = N; i > 0; i--){
+        for(j = 0; j < i-1; j++){
+           if(t[j].numero > t[j+1].numero){
+            Aluno aux = t[j];
+            t[j] = t[j+1];
+            t[j+1] = aux;
+            } 
+        }
+    }
+}
---
+++ Fichas/Ficha5/Alunos.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct aluno{
+    int numero;
+    char nome[100];
+    int miniT[6];
+    float teste;
+} Aluno;
